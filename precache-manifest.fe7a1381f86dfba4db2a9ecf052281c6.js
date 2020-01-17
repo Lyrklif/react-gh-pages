754d85645897c8f4d6b4de3988f9de6d05235ecb
Lyrklif <kochurow121@gmail.com>
2020-01-17 11:15:51 +0400

    typography-editor

---
+++ precache-manifest.fe7a1381f86dfba4db2a9ecf052281c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627bdca0369addff4c46d221f4f7ceac",
+    "url": "/typography-editor/react-gh-pages/index.html"
+  },
+  {
+    "revision": "a2c4f50d50259c9cf72c",
+    "url": "/typography-editor/react-gh-pages/static/css/main.3d783e58.chunk.css"
+  },
+  {
+    "revision": "f215dbdbe356877eba80",
+    "url": "/typography-editor/react-gh-pages/static/js/2.a8e4e045.chunk.js"
+  },
+  {
+    "revision": "1d18f4c3de57cbb9bac2a2f25e0142a0",
+    "url": "/typography-editor/react-gh-pages/static/js/2.a8e4e045.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2c4f50d50259c9cf72c",
+    "url": "/typography-editor/react-gh-pages/static/js/main.395b3957.chunk.js"
+  },
+  {
+    "revision": "f6283aa0b05d35ebd056",
+    "url": "/typography-editor/react-gh-pages/static/js/runtime-main.863085be.js"
+  }
+]);
