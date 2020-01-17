e029eabaac2ca860b55e92aa4042c76e3e30425d
Lyrklif <kochurow121@gmail.com>
2020-01-17 14:26:03 +0400

    task 36kqq6

--- build/precache-manifest.b5c5b7e4be29315c245145a837446b6f.js
+++ build/precache-manifest.b5c5b7e4be29315c245145a837446b6f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d046cc7b64567813ab243659347ad161",
+    "revision": "2038743a28830c7463cf50259575b43d",
     "url": "./index.html"
   },
   {
-    "revision": "e95fb61c6bb11d90598d",
-    "url": "./static/css/main.c1dd8ecc.chunk.css"
+    "revision": "63d57089cb3c193d8645",
+    "url": "./static/css/main.decd4050.chunk.css"
   },
   {
     "revision": "02d152971db90ea88880",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.66ca8dd0.chunk.js.LICENSE"
   },
   {
-    "revision": "e95fb61c6bb11d90598d",
-    "url": "./static/js/main.10788ac7.chunk.js"
+    "revision": "63d57089cb3c193d8645",
+    "url": "./static/js/main.a4cb655b.chunk.js"
   },
   {
     "revision": "1c20036138a5e7d0b4d4",
