790b5da2fc8276bac6b5cdb33bc6c5114f133636
Lyrklif <kochurow121@gmail.com>
2020-01-20 15:55:35 +0400

    ver 3

---
+++ build/precache-manifest.5b012ffc67d3b09f23efe46d9c59bafd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f0571f60762bab25bacb35b1f4b5b7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "20db5f99e75e3f08eb03",
+    "url": "./static/css/main.decd4050.chunk.css"
+  },
+  {
+    "revision": "2796f5a9a1067ca3fffc",
+    "url": "./static/js/2.60c89ba0.chunk.js"
+  },
+  {
+    "revision": "1d18f4c3de57cbb9bac2a2f25e0142a0",
+    "url": "./static/js/2.60c89ba0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20db5f99e75e3f08eb03",
+    "url": "./static/js/main.75ace550.chunk.js"
+  },
+  {
+    "revision": "1c20036138a5e7d0b4d4",
+    "url": "./static/js/runtime-main.32edbf5e.js"
+  }
+]);
